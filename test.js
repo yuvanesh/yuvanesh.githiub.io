f79c92c555cb45d0ce933a9a9530e9131720ef51
yuvanesh <yuvanesh@users.noreply.github.com>
2017-06-17 08:30:57 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>hhhhhhh</title>
+	<title>hhhhhhhdd</title>
   <script type="text/javascript">
-    alert(08888888);
-    window.addEventListener("load", function() {
-    alert(08888888);
-  });
+    	alert(08888888);
+    	window.addEventListener("load", function() {
+    		alert(08888888);
+  	});
   </script>
   <link rel="import" href="http://example.com/elements.html">
-	</head>
+  </head>
 <body>
 
 </body>
