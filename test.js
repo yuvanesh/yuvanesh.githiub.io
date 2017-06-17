a419bc08b0e1982278a5bd6e3e587a0348a1c438
yuvanesh <yuvanesh@users.noreply.github.com>
2017-06-17 08:29:19 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
-// come onnnn
-alert(08888888);
-window.addEventListener("load", function() {
-  alert(08888888);
-});
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hhhhhhh</title>
+  <script type="text/javascript">
+    alert(08888888);
+    window.addEventListener("load", function() {
+    alert(08888888);
+  });
+  </script>
+  <link rel="import" href="http://example.com/elements.html">
+	</head>
+<body>
+
+</body>
+</html>
